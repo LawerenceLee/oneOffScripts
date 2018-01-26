4214904fd88f8e6a7370faa876a875b4909ba2b2
Lawerence Lee <LawerenceLee@users.noreply.github.com>
2018-01-26 07:32:18 -0800

    Delete get_py_books.py

--- get_py_books.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    username = os.getlogin()
-    os.chdir('/Users/{}/Desktop'.format(username))
-    os.system('git clone https://LawerenceLee@bitbucket.org/LawerenceLee/python_books.git')
