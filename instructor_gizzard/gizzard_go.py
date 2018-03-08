2b95cc2f05f6b8af890b6d3353c011e27e7e96de
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-08 06:41:14 -0800

    initial commit

---
+++ instructor_gizzard/gizzard_go.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+
+# NEEDS LOGGING
+
+def check_email():
+    """
+    Check email for instructions and scripts to run in bitbucket repostory.
+    """
+    # check inbox for instruction emails
+    # email_instructions = []
+        # If emails:
+            # for email in emails: 
+                # parse email text for instructions
+                # email_instructions.append((subject, body))
+            # return email_insturctions
+        # else:
+            # exit
+    pass
+
+
+def parse_instructions(instructions):
+    # perform a git pull
+    # scripts_list = []
+    # grab a list of all scripts in directory
+    # for instruction in instructions:
+        # subject, body = instruction
+        # parse subject and grab script name
+        # check if script in script_dir
+            # check for vars in body:
+                # save vars to a py file (e.g. <script_name>_VARS.py)
+            # save script_name to scripts_list
+    # return scripts_list
+    pass
+
+
+def execute_instructions(scripts):
+    # for script in scripts:
+        # type = script.split('.')[1]
+        # check type:
+            # if python:
+                # os.system('python {}'.format(script))
+    pass
